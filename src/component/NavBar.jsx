b76a8b4d7abb35f38e54b5f77bec76d9247534aa
PiaCarlos <carlos.andres.rosoha.alvarado@gmail.com>
2025-09-04 17:04:31 -0400

    Add files via upload

---
+++ src/component/NavBar.jsx
@@ -0,0 +1,10 @@
+export function NavBar() {
+    return (
+        <header>
+            <nav className="nav-items-css">
+                <img className= "react-logo" src="react-logo.png" alt="React logo" />
+                <span className="title"> Reacts Facts</span>
+            </nav>
+        </header> 
+    )
+}
